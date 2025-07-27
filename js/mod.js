681bdd71f81da140d9c8a7067f4136b9dae0efbd
sanraaaaaaaaaaaa <sarasnowling@yahoo.com>
2025-07-27 15:06:54 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	author: "nobody",
-	pointsName: "points",
+	name: "The ElemenTree",
+	author: "uselessguest",
+	pointsName: "Points",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
